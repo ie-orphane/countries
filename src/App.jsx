b7778275b9b997e22de226a72eba47741d0fe0ad
ie-orphane <ilyasselyatime0@gmail.com>
2024-01-10 12:37:14 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/country/home" element={<Home />} />
         <Route path="/country/:code" element={<Country />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
